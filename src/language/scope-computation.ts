b02f5c0621bfeb0d19bc5d11b2210e7464312bac
Marian Baba <marianbaba16@gmail.com>
2023-05-30 12:02:54 +0200

    principal cross references seem to work

--- src/language/scope-computation.ts
+++ src/language/scope-computation.ts
@@ -1,6 +1,7 @@
-import { AstNodeDescription, DefaultScopeComputation, LangiumDocument, LangiumServices, Module, PartialLangiumServices, streamAllContents } from "langium";
-import { isPrincipals } from "./generated/ast";
-import { HelloWorldServices } from "./hello-world-module";
+import { AstNode, AstNodeDescription, DefaultScopeComputation, LangiumDocument, LangiumServices, Module, MultiMap, PartialLangiumServices, PrecomputedScopes, streamAllContents } from "langium"
+import { isPrincipal } from "./generated/ast"
+import { HelloWorldAddedServices, HelloWorldServices } from "./hello-world-module"
+import { HelloWorldValidator } from "./hello-world-validator"
 
 export class HelloWorldScopeComputation extends DefaultScopeComputation {
     constructor(services: LangiumServices) {
@@ -8,19 +9,31 @@ export class HelloWorldScopeComputation extends DefaultScopeComputation {
     }
 
     override async computeExports(document: LangiumDocument): Promise<AstNodeDescription[]> {
-        const exportedNames: AstNodeDescription[] = []
-        for (const childNode of streamAllContents(document.parseResult.value)) {
-            if (isPrincipals(childNode)) {
-                childNode.elements.forEach(principal => {
-                    exportedNames.push(this.descriptions.createDescription(document.parseResult.value, principal.name, document))
-                })
+        const exportedDescriptions: AstNodeDescription[] = []
+        for (const childNode in streamAllContents(document.parseResult.value)) {
+            if (isPrincipal(childNode)) {
+               // exporting every principal name
+                exportedDescriptions.push(this.descriptions.createDescription(childNode, childNode.name))
             }
         }
-        return exportedNames
+        return exportedDescriptions
     }
+
+    override async computeLocalScopes(document: LangiumDocument): Promise<PrecomputedScopes> {
+        const model = document.parseResult.value
+        const scopes = new MultiMap<AstNode, AstNodeDescription>()
+        streamAllContents(model).filter(isPrincipal).forEach(principal => {
+            scopes.add(model, this.descriptions.createDescription(principal, principal.name, document))
+        })
+        return scopes
+    }
+    
 }
 
-export const HelloWorldModule: Module<HelloWorldServices, PartialLangiumServices> = {
+export const HelloWorldModule: Module<HelloWorldServices, PartialLangiumServices & HelloWorldAddedServices> = {
+    validation: {
+        HelloWorldValidator: HelloWorldValidator
+    },
     references: {
         ScopeComputation: (services) => new HelloWorldScopeComputation(services)
     }
